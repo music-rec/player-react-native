22d5ad7b2616355cbafc7dc80c25240760f85058
dctalbot <dctalbot@umich.edu>
2018-07-20 00:53:53 -0500

    add time to song component

--- js/components/Song.js
+++ js/components/Song.js
@@ -1,20 +1,24 @@
-/*
- * @flow
- */
-
 import React from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native'
-import type { SongT } from '../models'
 
-type Props = {
-  data: SongT
-}
+export default class Song extends React.PureComponent {
+  renderLabel() {
+    if (this.props.data.label && this.props.data.year) {
+      return (
+        <Text style={styles.label}>
+          {this.props.data.label} ({this.props.data.year})
+        </Text>
+      )
+    }
+  }
 
-export default class Song extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.name}>{this.props.data.name}</Text>
+        <Text style={styles.time}>{this.props.data.at}</Text>
+        <Text style={styles.name}>
+          {this.props.data.artist}: “{this.props.data.name}”
+        </Text>
       </View>
     )
   }
@@ -23,9 +27,22 @@ export default class Song extends React.Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'whitesmoke'
+    backgroundColor: 'whitesmoke',
+    height: 50,
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  time: {
+    fontSize: 10
   },
   name: {
     color: 'black'
+  },
+  label: {
+    fontStyle: 'italic',
+    fontSize: 13
   }
 })
